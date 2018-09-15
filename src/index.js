c004aa5ce3e185793ec245bbfe3086f736895296
Carlos A. Diaz <carlos4diazj@hotmail.com>
2018-09-15 01:45:57 -0500

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import _ from 'lodash'
+import printMe from './print'
 
 function component() {
     let element = document.createElement('div')
+    let btn = document.createElement('button')
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+    btn.innerHTML = 'ClickMe'
+    btn.onclick = printMe
+    element.appendChild(btn)
     return element
 }
 
